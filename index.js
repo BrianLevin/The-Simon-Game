44786c4333b79b6f51f21eb1874aa5849213c1a5
BrianLevin <bml201095@gmail.com>
2021-04-14 11:00:52 -0400

    pushed randomchosencolor to the game pattern

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var buttonColors = ["red", "blue", "green", "yellow",]
+var gamePattern= [];
+
+gamePattern.push(randomChosenColor);
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+ var randomChosenColor =buttonColours[randomNumber]; 
+
 
 function nextSequence(){
 
